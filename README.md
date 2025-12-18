f2cab6a3029e1385c9d9af89077e02c94cfc142f
Mrinaal Dogra <azadmrinaal@gmail.com>
2025-12-17 19:19:02 -0800

    Add python requirements; Add llama3.1 test file

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
-# pdf-chat
+# PDF Chat
+
+A self-project to implement RAG pipeline, where we can chat with any uploaded PDFs
+
+Install `ollama`
+
+```sh
+# This pulls the 8B parameter model (approx 4.7GB).
+ollama pull llama3.1
+```
---
+++ test_ollama.py
@@ -0,0 +1,23 @@
+import asyncio
+from ollama import AsyncClient
+
+
+OLLAMA_MODEL_NAME = "llama3.1"
+OLLAMA_MODEL_TAG = "latest"
+# OLLAMA_MODEL = f"{OLLAMA_MODEL_NAME}:{OLLAMA_MODEL_TAG}"
+
+OLLAMA_MODEL = "llama3.1"
+
+async def get_joke():
+    message = {'role': 'user', 'content': 'Tell me a joke!'}
+    response = await AsyncClient().chat(model=OLLAMA_MODEL, messages=[message])
+
+    print("="*50)
+    print(response)
+    print("="*50)
+    print(response.message.content)
+    print("="*50)
+
+
+if __name__ == "__main__":
+    asyncio.run(get_joke())
